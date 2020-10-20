377300be632a0eda997b5ced9e55e193b08f3c3f
Paraskevas_Lysikatos <vaslysalex@hotmail.gr>
2020-10-20 16:52:56 +0300

    Test patient, crud test doctor

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PatientComponent } from './patient/patient.component';
+import{DoctorComponent} from './doctor/doctor.component'
 
 const routes: Routes = 
 [
   {path: '', component: LoginComponent},
   {path:'register',component: RegisterComponent},
   {path:'patient',component: PatientComponent},
+  {path:'doctor',component: DoctorComponent},
   
 ];
 
