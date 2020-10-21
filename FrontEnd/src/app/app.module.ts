8df02e22bd706ec083fa579fda6db90252b067ad
Paraskevas_Lysikatos <vaslysalex@hotmail.gr>
2020-10-21 22:59:39 +0300

    Revert "Revert "Test patient get- post response,test doctor, anastasia management""
    This reverts commit 1c29085f987937913e077649f50d86547c6fd1ea.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,24 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PatientComponent } from './patient/patient.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PatientService } from './services/patient.service';
+import { DoctorComponent } from './doctor/doctor.component';
+import { DoctorService } from './services/doctor.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
+    HomeComponent,
     LoginComponent,
-    PatientComponent
+    RegisterComponent,
+    PatientComponent,
+    DoctorComponent,
+    AdminComponent
+ 
+  
+  
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+    
+    ],
+
+  providers: [PatientService,DoctorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 @Component({
-  selector: 'app-login',
+  selector: 'codehub-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private _router: Router) { }
-
-  RegisterNavClick() {
-    this._router.navigate(['/register'])
-  }
-
-  PatientNavClick() {
-    this._router.navigate(['/patient'])
-  }
-
-
+  loginForm:FormGroup;
+  submitted = false;
+  
+  constructor(private formBuilder: FormBuilder,private _router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm= this.formBuilder.group({
+      userName:['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    
+    },);
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+          return;
+      }
+
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value))
+      this._router.navigate(['/patient'])
   }
 
+  BackClick() {
+    this._router.navigate(['/home'])
+  }
+  
 }
