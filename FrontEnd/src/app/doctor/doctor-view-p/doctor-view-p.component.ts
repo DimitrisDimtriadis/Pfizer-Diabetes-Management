93e81305e0ce8dde0ed2907a31e33454ae54bf4f
Paraskevas_Lysikatos <vaslysalex@hotmail.gr>
2020-10-29 20:32:45 +0200

    fixes something

--- FrontEnd/src/app/doctor/doctor-view-p/doctor-view-p.component.ts
+++ FrontEnd/src/app/doctor/doctor-view-p/doctor-view-p.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import {Chart} from '../../../../node_modules/chart.js/dist/chart.js';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-view-p',
@@ -10,42 +13,19 @@ import {Chart} from '../../../../node_modules/chart.js/dist/chart.js';
 })
 export class DoctorViewPComponent implements OnInit {
   chart=[];
-
   userForm:FormGroup;
-    submitted = false;
-  
-    constructor(private formBuilder: FormBuilder,private _router: Router) { }
-    
-    ngOnInit(): void {
-      this.userForm= this.formBuilder.group({
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder,private _router: Router) { }
+
+  ngOnInit(): void {
+
+    this.userForm= this.formBuilder.group({
        
         email:['', [Validators.required, Validators.email]],
         dEnd :['', Validators.required],
         dStart :['', Validators.required]
       });
-    }
-    // convenience getter for easy access to form fields
-    get f() { return this.userForm.controls; }
-  
-    onSubmit() {
-        this.submitted = true;
-  
-        // stop here if form is invalid
-        if (this.userForm.invalid) {
-            return;
-        }
-  
-       
-    }
-  
-    numberOnly(event): boolean {
-      const charCode = (event.which) ? event.which : event.keyCode;
-      if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-      }
-      return true;
-  
-    
 
     this.chart = new Chart('myChart', {
       type: 'line',
@@ -82,6 +62,27 @@ export class DoctorViewPComponent implements OnInit {
 
   }
 
+  get f() { return this.userForm.controls; }
   
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.userForm.invalid) {
+          return;
+      }
+
+     
+  }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
+
 
 }
